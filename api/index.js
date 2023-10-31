8fcb54ed834d4059aa9223a712772a11994dac40
Akram <hfaiedhakram@gmail.com>
2023-10-31 01:14:04 +0100

    added dotenv and connection to mongoDB

--- api/index.js
+++ api/index.js
@@ -1,10 +1,18 @@
 import express from 'express'
+import mongoose from 'mongoose';
+import 'dotenv/config'
 
+
+
+mongoose.connect(process.env.DATABASE_URL).then(
+    () => { console.log('🎟  - Connected to mongoDB  - 🎟 '); },
+    err => { console.log('Intial Conection to mongoDB error:' + err); }
+)
 
 const app = express()
 
 
 
 app.listen(3000, () => {
-    console.log("🚀  Server is running on Port 3000:");
+    console.log("🚀 - Server is running on Port 3000! - 🚀 ");
 })

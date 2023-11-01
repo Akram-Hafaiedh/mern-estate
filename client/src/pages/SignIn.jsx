0cf95c52e9ecfb2503b8074b9ff01ecee4cd9350
Akram <hfaiedhakram@gmail.com>
2023-11-02 00:11:16 +0100

    added redux to have userData globally usable in the app

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,12 +1,16 @@
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux';
+import { signInStart, signInSucces, signInFailure } from '../redux/features/user/userSlice'
 
 export default function SignIn() {
 
     const [formData, setFormData] = useState({})
-    const [error, setError] = useState(null);
-    const [loading, setLoading] = useState(false);
+    const { loading, error } = useSelector((state) => state.user)
+    // const [error, setError] = useState(null);
+    // const [loading, setLoading] = useState(false);
     const navigate = useNavigate();
+    const dispatch = useDispatch();
 
     function handleChange(e) {
         setFormData(
@@ -20,7 +24,8 @@ export default function SignIn() {
         console.log(formData);
         // const res = await fetch('/api/auth/signup', formData)
 
-        setLoading(true);
+        // setLoading(true);
+        dispatch(signInStart());
         const res = await fetch('/api/auth/signin', {
             method: "POST", //*GET, POST, PUT, DELETE, etc
             headers: {
@@ -32,12 +37,14 @@ export default function SignIn() {
         const data = await res.json(); //* convert the response to JSON
 
         if (data.success === false) {
-            setError(data.message);
-            setLoading(false);
+            // setError(data.message);
+            // setLoading(false);
+            dispatch(signInFailure(data.message));
             return;
         }
-        setLoading(false);
-        setError(null);
+        // setLoading(false);
+        // setError(null);
+        dispatch(signInSucces(data))
         navigate('/')
         console.log(data);
     }
